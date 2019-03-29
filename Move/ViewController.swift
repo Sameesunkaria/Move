d49c66b498e2bad8f36e5b4a934657bc0a90447f
Samar Sunkaria <sameesunkaria@me.com>
2019-03-29 11:55:51 +0100

    Initial testing

--- Move/ViewController.swift
+++ Move/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import CoreMotion
+
+struct Vector {
+    var x: Double
+    var y: Double
+    var z: Double
+
+    static let zero = Vector(x: 0, y: 0, z: 0)
+}
 
 class ViewController: UIViewController {
 
+    let manager = CMMotionManager()
+    var vector = Vector.zero
+
+    var lastThreshold = Date()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        guard manager.isAccelerometerAvailable else { fatalError("Accelerometer not available") }
+
+        manager.deviceMotionUpdateInterval = 0.01
+
+        manager.startDeviceMotionUpdates(to: .main) {
+            [weak self] (data, error) in
+
+            guard let self = self, let data = data, error == nil else {
+                return
+            }
+
+            print("\(data.userAcceleration.x), \(data.userAcceleration.y), \(data.userAcceleration.z)")
+        }
+
     }
 
 
