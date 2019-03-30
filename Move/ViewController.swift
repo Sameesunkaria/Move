e5f9e46bc5b3aae0e5194d8552c9db6766a1d8b1
Zofia Drabek <zosia.drabek@gmail.com>
2019-03-30 15:20:55 +0100

    Add running buffer

--- Move/ViewController.swift
+++ Move/ViewController.swift
@@ -23,22 +23,61 @@ class ViewController: UIViewController {
     var vector = Vector.zero
 
     var lastThreshold = Date()
+    let sampleInterval = 1 / 50.0
+    let accelerationAlongGravityBuffer = RunningBuffer(size: 50)
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         guard manager.isAccelerometerAvailable else { fatalError("Accelerometer not available") }
 
-        manager.deviceMotionUpdateInterval = 0.01
+        manager.deviceMotionUpdateInterval = sampleInterval
 
-        manager.startDeviceMotionUpdates(to: .main) {
+        manager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical, to: .main) {
             [weak self] (data, error) in
 
             guard let self = self, let data = data, error == nil else {
                 return
             }
+            
+            let horizontalVector = data.magneticField
+            
+            let accelerationAlongGravity =
+                data.userAcceleration.x * data.gravity.x +
+                data.userAcceleration.y * data.gravity.y +
+                data.userAcceleration.z * data.gravity.z
+            
+            let accelerationMagnitude =
+                pow(data.userAcceleration.x, 2) +
+                pow(data.userAcceleration.y, 2) +
+                pow(data.userAcceleration.z, 2)
+            
+            let accelerationPerpendicularToGravity = sqrt(accelerationMagnitude - pow(accelerationAlongGravity, 2))
+            
+            self.accelerationAlongGravityBuffer.addSample(accelerationAlongGravity)
 
-            print("\(data.userAcceleration.x), \(data.userAcceleration.y), \(data.userAcceleration.z)")
+            
+            if !self.accelerationAlongGravityBuffer.isFull() {
+                return
+            }
+            
+            let accumulatedVelocityAlongGravity = self.accelerationAlongGravityBuffer.sum() * self.sampleInterval
+//            let accumulatedVelocityPerpendicularToGravity =
+
+            
+            
+//            let peakAcceleration = accumulatedVelocity > 0 ? self.accelerationAlongZBuffer.max() : self.accelerationAlongZBuffer.min()
+            
+//            print("accumulated velocity: \(accumulatedVelocity)")
+//            print("peak acceleration: \(peakAcceleration)")
+            
+//            print("\(accumulatedVelocityAlongGravity), \(accelerationAlongGravity), \(horizontalVector)")
+            print("\(data.attitude.yaw), \(data.attitude.pitch), \(data.attitude.roll)")
+            
+//            if (accumulatedVelocity > velocityTreshold && peakAcceleration > acceleration) {
+//                print("up movement")
+//            }
         }
 
     }
